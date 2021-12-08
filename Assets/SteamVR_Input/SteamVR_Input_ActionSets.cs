4700557bf7c9085737569e5b11785c4f9b434816
TelAr <qet112313@gmail.com>
2021-12-09 04:03:31 +0900

    VR 세팅 변경

--- Assets/SteamVR_Input/SteamVR_Input_ActionSets.cs
+++ Assets/SteamVR_Input/SteamVR_Input_ActionSets.cs
@@ -23,6 +23,8 @@ namespace Valve.VR
         
         private static SteamVR_Input_ActionSet_mixedreality p_mixedreality;
         
+        private static SteamVR_Input_ActionSet_NewSet p_NewSet;
+        
         public static SteamVR_Input_ActionSet_default _default
         {
             get
@@ -47,15 +49,25 @@ namespace Valve.VR
             }
         }
         
+        public static SteamVR_Input_ActionSet_NewSet NewSet
+        {
+            get
+            {
+                return SteamVR_Actions.p_NewSet.GetCopy<SteamVR_Input_ActionSet_NewSet>();
+            }
+        }
+        
         private static void StartPreInitActionSets()
         {
             SteamVR_Actions.p__default = ((SteamVR_Input_ActionSet_default)(SteamVR_ActionSet.Create<SteamVR_Input_ActionSet_default>("/actions/default")));
             SteamVR_Actions.p_buggy = ((SteamVR_Input_ActionSet_buggy)(SteamVR_ActionSet.Create<SteamVR_Input_ActionSet_buggy>("/actions/buggy")));
             SteamVR_Actions.p_mixedreality = ((SteamVR_Input_ActionSet_mixedreality)(SteamVR_ActionSet.Create<SteamVR_Input_ActionSet_mixedreality>("/actions/mixedreality")));
+            SteamVR_Actions.p_NewSet = ((SteamVR_Input_ActionSet_NewSet)(SteamVR_ActionSet.Create<SteamVR_Input_ActionSet_NewSet>("/actions/NewSet")));
             Valve.VR.SteamVR_Input.actionSets = new Valve.VR.SteamVR_ActionSet[] {
                     SteamVR_Actions._default,
                     SteamVR_Actions.buggy,
-                    SteamVR_Actions.mixedreality};
+                    SteamVR_Actions.mixedreality,
+                    SteamVR_Actions.NewSet};
         }
     }
 }
