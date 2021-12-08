3b30fc70c59139f54c354a0ea151a6ae8805c40c
TelAr <qet112313@gmail.com>
2021-12-08 23:57:44 +0900

    비행기 모션 수정 및 아이템 획득 효과음, 버그 상황 교정

--- Assets/Script/ForPlanes.cs
+++ Assets/Script/ForPlanes.cs
@@ -5,21 +5,27 @@ using UnityEngine;
 public class ForPlanes : MonoBehaviour
 
 {
-    public float speed = 3;
-    public float radius = 0.1f;
+    public float speed;
+    public float radius;
+    public float start_rotation = 0;
     private float runningTime = 0;
+    private Vector3 origin;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        origin = gameObject.transform.localPosition;
+        runningTime = Mathf.Deg2Rad * start_rotation;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         runningTime += Time.deltaTime * speed;
-        Vector3 moveDir = new Vector3(radius * Mathf.Cos(runningTime), 0.0f, radius * Mathf.Sin(runningTime));
-        transform.Translate(moveDir);
+        Vector3 moveDir = new Vector3(radius * Mathf.Cos(runningTime), 0.0f, radius * Mathf.Sin(runningTime))+ origin;
+        Vector3 rot = new Vector3(0, -Mathf.Rad2Deg * runningTime+90, 0);
+        transform.localPosition=moveDir;
+        //transform.Translate(moveDir);
+        transform.rotation = Quaternion.Euler(rot);
     }
 }
